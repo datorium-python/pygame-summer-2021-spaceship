ac91a62e570a20a0135a39242764bb9e7ff5bbab
mpilscikov <m.pilscikov@gmail.com>
2021-08-09 19:01:58 +0300

    WIP

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import pygame
 class Spaceship:
 
     image = pygame.image.load('spaceship.png')
-    speed = 0.9
+    speed = 0.7
 
     def __init__(self, x, y):
         
@@ -15,16 +15,16 @@ class Spaceship:
 
     def move(self, window, keys):
         
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] and self.y_position > 0:
             self.y_position -= Spaceship.speed
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] and self.x_position > 0:
             self.x_position -= Spaceship.speed
 
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] and self.y_position < window.get_height() - Spaceship.image.get_height():
             self.y_position += Spaceship.speed
 
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and self.x_position < window.get_width()  - Spaceship.image.get_width():
             self.x_position += Spaceship.speed
 
     def draw(self, window):
